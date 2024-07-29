e266ad11b0419458ce7dfd813393573a25d8509d
rvalgreen <ricardovalverde2000@gmail.com>
2024-07-29 23:04:11 +0100

    site skeleton

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template >
+  <header class="flex justify-center bg-emerald-900 p-3 text-stone-300">
+      <nav class="flex justify-start w-full lg:w-10/12">
+        <RouterLink to="/" class="pr-2">Home</RouterLink>
+        <RouterLink to="/about"  class="">About</RouterLink>
+      </nav>
+  </header>
+
+  <main class="flex justify-center pt-2 px-3">
+    <RouterView class="w-full mx-4 lg:w-10/12 bg-blue-300 bg-opacity-10"/>
+  </main>
+
+</template>
+
+<style>
+@import 'App.css';
+</style>
