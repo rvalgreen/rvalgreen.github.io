6fa86b4666a1bb3e1975e9c79a5e8f3b40b8b2d7
Ricardo Valverde <ricardovalverde2000@gmail.com>
2026-08-21 16:30:56 +0100

    more visual improvements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="px-8 sm:px-16 py-4 flex items-center justify-around sm:justify-center gap-x-8">
+  <div class="px-8 sm:px-16 py-4 flex flex-col-reverse sm:flex-row items-center justify-center gap-x-8 gap-y-4">
 
-    <div class="flex flex-col justify-center text-cyan-700/65">
-      <div class="text-xl sm:text-4xl font-bold">Ricardo Valverde Lopes</div>
-      <div class="text-lg sm:text-lg font-semibold mb-4">Software Dev @ Cult of Bits</div>
+    <div class="flex flex-col justify-center text-center sm:text-left">
+      <div class="text-xl sm:text-4xl font-bold text-cyan-700">Ricardo Valverde Lopes</div>
+      <div class="text-lg sm:text-lg font-semibold mb-4 text-cyan-700">Software Dev @ Cult of Bits</div>
 
-      <div class="flex gap-3">
-        <!-- Email -->
-        <a href="mailto:your@email.com"
-          class="text-base md:text-xl shadow-md bg-blue-500 flex items-center justify-center px-2 py-1 rounded hover:bg-blue-600 transition"
-          aria-label="Email">
-          <i class="fas fa-envelope text-white"></i>
-        </a>
-
+      <div class="flex gap-3 justify-center sm:justify-start">
         <!-- LinkedIn -->
         <a href="https://www.linkedin.com/in/r-valverde" target="_blank"
           class="text-base md:text-xl shadow-md bg-blue-700 flex items-center justify-center px-2 py-1 rounded hover:bg-blue-600 transition"
@@ -22,7 +15,7 @@
 
         <!-- GitHub -->
         <a href="https://github.com/rvalgreen" target="_blank"
-          class="text-base md:text-xl shadow-md bg-gray-600  flex items-center justify-center px-2 py-1 rounded hover:bg-gray-700 transition"
+          class="text-base md:text-xl shadow-md bg-gray-600 flex items-center justify-center px-2 py-1 rounded hover:bg-gray-700 transition"
           aria-label="GitHub">
           <i class="fab fa-github text-white "></i>
         </a>
@@ -30,7 +23,8 @@
     </div>
 
     <div class="">
-      <img src="/myphoto.jpg" alt="Your Photo" class="w-48 sm:w-64 aspect-square rounded-full object-cover shadow-lg" />
+      <img src="/myphoto.jpg" alt="Your Photo"
+        class="w-48 sm:w-64 aspect-square rounded-full object-cover shadow-lg ring-4 ring-cyan-600/20 ring-offset-2 ring-offset-white" />
     </div>
 
   </div>
